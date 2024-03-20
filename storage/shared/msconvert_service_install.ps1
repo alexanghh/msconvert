764592e9af91a630bc03f03222269b52d11a7ddf
Alex Ang HH <alexanghh@gmail.com>
2024-03-20 18:13:31 +0800

    Modified service to start fastapi app instead of flask

--- storage/shared/msconvert_service_install.ps1
+++ storage/shared/msconvert_service_install.ps1
@@ -1,5 +1,6 @@
 \\host.lan\Data\nssm install msconvert C:\Users\Docker\AppData\Local\Programs\Python\Python312\python.exe
-\\host.lan\Data\nssm set msconvert AppParameters '-m flask run --host 0.0.0.0'
+#\\host.lan\Data\nssm set msconvert AppParameters '-m flask run --host 0.0.0.0'
+\\host.lan\Data\nssm set msconvert AppParameters '-m uvicorn main:app --port 5000 --host 0.0.0.0'
 \\host.lan\Data\nssm set msconvert AppDirectory \\host.lan\Data\msconvert\
 \\host.lan\Data\nssm set msconvert AppStdout \\host.lan\Data\msconvert\msconvert.log
 \\host.lan\Data\nssm set msconvert AppStderr \\host.lan\Data\msconvert\msconvert.log
